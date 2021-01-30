d43fae26e8a9824d13eeede1e7d1d1f0f9baad91
miguelcoria94 <miguelcoria94@gmail.com>
2021-01-30 10:46:07 -0600

    error message need to install babel

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,1 +1,10 @@
 console.log("app is running")
+
+
+
+// JSX - Javascript XML
+
+const template = <p>This is JSX from app.js!</p>;
+const appRoot = document.getElementById("app")
+
+ReactDOM.render(template, appRoot)
