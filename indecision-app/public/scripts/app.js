70a1a5ab0d762e7fb3816f959abae728601de025
miguelcoria94 <miguelcoria94@gmail.com>
2021-02-03 12:30:29 -0600

    numer is working

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -12,37 +12,53 @@ var app = {
 
 var submitForm = function submitForm(e) {
     e.preventDefault();
+
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderApp();
+    }
 };
 
-var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
+var renderApp = function renderApp() {
+    var template = React.createElement(
+        "div",
         null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        "p",
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        "p",
-        null,
-        app.options.length > 0 ? "Here are your options" : "You have no options"
-    ),
-    React.createElement(
-        "form",
-        { onSubmit: submitForm },
-        React.createElement("input", { type: "text", name: "option" }),
         React.createElement(
-            "button",
-            { type: "submit" },
-            "Add Option"
+            "h1",
+            null,
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            "p",
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            "p",
+            null,
+            app.options.length > 0 ? "Here are your options" : "You have no options"
+        ),
+        React.createElement(
+            "p",
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            "form",
+            { onSubmit: submitForm },
+            React.createElement("input", { type: "text", name: "option" }),
+            React.createElement(
+                "button",
+                { type: "submit" },
+                "Add Option"
+            )
         )
-    )
-);
+    );
+
+    ReactDOM.render(template, appRoot);
+};
 
 var appRoot = document.querySelector("#app");
-ReactDOM.render(template, appRoot);
+renderApp();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -10,19 +10,37 @@ const app = {
 
 const submitForm = (e) => {
     e.preventDefault()
+
+    const option = e.target.elements.option.value
+    if (option) {
+        app.options.push(option)
+        e.target.elements.option.value = ''
+        renderApp()
+    }
 }
 
-const template = (
-    <div>
+const renderApp = () => {
+    const template = (
+      <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? "Here are your options" : "You have no options"}</p>
+        <p>
+          {app.options.length > 0
+            ? "Here are your options"
+            : "You have no options"}
+        </p>
+        <p>{app.options.length}</p>
         <form onSubmit={submitForm}>
-            <input type="text" name="option"></input>
-            <button type="submit">Add Option</button>
+          <input type="text" name="option"></input>
+          <button type="submit">Add Option</button>
         </form>
-    </div>
-);
+      </div>
+    );
+
+    ReactDOM.render(template, appRoot);
+}
+
+
 
 const appRoot = document.querySelector("#app")
-ReactDOM.render(template, appRoot);
+renderApp();
